ec22a620e7929cbe7b1a42043bc88d29e83d7f65
Aaron LaBeau <aaron.lebeau@ey.com>
2019-05-11 22:53:32 -0400

    initial check-in directions

--- Forms/Mobile.RefApp.CoreUI/ViewModels/Azure/AzureTokenGeneratorViewModel.cs
+++ Forms/Mobile.RefApp.CoreUI/ViewModels/Azure/AzureTokenGeneratorViewModel.cs
@@ -18,6 +18,64 @@ namespace Mobile.RefApp.CoreUI.ViewModels
         private readonly IAzureAuthenticatorEndpointService _authenticationService;
         private readonly IEndpointService _endpointService;
 
+        private string _appId;
+        public string AppId
+        {
+            get => _appId;
+            set => SetProperty(ref _appId, value);
+        }
+
+        private string _tentantUri;
+        public string TentantUri
+        {
+            get => _tentantUri;
+            set => SetProperty(ref _tentantUri, value);
+        }
+
+        private string _audience;
+        public string Audience
+        {
+            get => _audience;
+            set => SetProperty(ref _audience, value);
+        }
+
+        private string _redirectUri;
+        public string RedirectUri
+        {
+            get => _redirectUri;
+            set => SetProperty(ref _redirectUri, value);
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        private bool _useBroker;
+        public bool UseBroker
+        {
+            get => _useBroker;
+            set => SetProperty(ref _useBroker, value);
+        }
+
+        private Endpoint _selectedEndpoint;
+        public Endpoint SelectedEndpoint
+        {
+            get => _selectedEndpoint;
+            set 
+            { 
+                SetProperty(ref _selectedEndpoint, value);
+
+                RedirectUri = _selectedEndpoint.RedirectUri;
+                Audience = _selectedEndpoint.ResourceId;
+                AppId = _selectedEndpoint.ApplicationId;
+                TentantUri = _selectedEndpoint.Authority;
+                UseBroker = _selectedEndpoint.UseBroker;
+            }
+        }
+
         public ObservableCollection<Endpoint> EndPoints { get; set; }
 
         public AzureTokenGeneratorViewModel(
@@ -31,6 +89,7 @@ namespace Mobile.RefApp.CoreUI.ViewModels
             EndPoints = new ObservableCollection<Endpoint>();
 
             Title = "Token Generator";
+            Status = "No Token";
         }
 
         public async override Task Initialize(Dictionary<string, object> navigationsParams = null)
@@ -41,6 +100,13 @@ namespace Mobile.RefApp.CoreUI.ViewModels
                 endpoints.ForEach(x => EndPoints.Add(x));
             }
             await base.Initialize(navigationsParams);
+        }
+
+        private void SetTokenStatusMessage(string firstName,
+                                 string lastName,
+                                 DateTimeOffset expires)
+        {
+            Status = $"Token Received for {firstName} {lastName} that expires on {expires}";
         }
     }
 }
